d8f49a5dc6f42f962d7d21fdbbe1bbacb031454c
tikismoke <webmaster@poudot.fr>
2020-06-15 22:37:21 +0200

    allow not only pi hdmi to turn on off

--- config.template.py
+++ config.template.py
@@ -3,5 +3,9 @@ mqtt_port = 1883
 mqtt_topic = "screen/rpi"
 mqtt_username = "USERNAME"
 mqtt_password = "PASSWORD"
+#On raspberry pi
 power_on_command = "vcgencmd display_power 1"
 power_off_command = "vcgencmd display_power 0"
+#On other hdmi linux device:
+#power_on_command = "xset -display :0 dpms force on"
+#power_off_command = "xset -display :0 dpms force off"
